60beaef4999ca6fbd1b5b870061f1669836bca6b
amanmaharj <aman.mahaj@gmail.com>
2022-04-19 21:00:07 -0600

    question 4,5,6

--- jsInterview.js
+++ jsInterview.js
@@ -31,7 +31,7 @@ num += 1; //operation plays an important role for the result
 
 console.log(num);
 
-// QUESTION 3
+                    // QUESTION 3
 
 //revealing module patterns
 //Turn this object literal into a module that only exposes the render method
@@ -69,3 +69,84 @@ myModule.render();
 
 //     }
 // };
+
+                    //  QUESTION 4
+//determine if the function received the number of params it expected
+
+
+let f = function (a, b) {
+
+    if (arguments.length === f.length) {
+        console.log('it is matched')
+    } else {
+        console.log('it is not matched')
+    }
+}
+           
+
+
+f(1);
+f(1, 2);
+f(1, 2, 3);
+                 //Question 5
+//ASI, object literals, square bracket syntax, hoisting
+//what happens when you run this code? why?
+//How would you fix this?
+
+let a = {
+    a: 123
+};//this semicolon is needed over here because the compiler need to first makesure that it is assigned that is 'a' object
+
+
+[a].forEach(function (x) {
+    console.log(x);
+})
+
+                            //QUESTION 6
+
+/**
+* Create an example of a JavaScript Singleton.
+* After the first object is created, it will return additional 
+* references to itself
+*/
+
+let obj = (function () {
+    let objInstance; //private variable
+    function create() { //private function to create methods and properties
+        let _isRunning = false;
+        let start = function () {
+            _isRunning = true;
+        }
+        let stop = function () {
+            _isRunning = false
+        }
+
+        let currentState = function () {
+            return _isRunning;
+        }
+
+        return {
+            start: start,
+            stop: stop,
+            currentState: currentState
+
+        }
+    }
+    return {
+        getInstance: function () {
+            if (!objInstance) { //this if sentence will create a singleton
+                objInstance = create();
+            }
+            return objInstance
+        }
+    };
+})();
+
+let obj1 = obj.getInstance();
+let obj2 = obj.getInstance();
+
+obj1.start();
+
+console.log(obj1.currentState());
+console.log(obj2.currentState());
+
